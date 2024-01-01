e298cd32afac6b3a719de733e49c00a62a22f3fa
halidak <halida.karisik6@gmail.com>
2024-01-01 23:59:12 +0100

    rating removed

--- ScienceFestival.REST.Gateway/Models/Show.cs
+++ ScienceFestival.REST.Gateway/Models/Show.cs
@@ -8,6 +8,5 @@
         public DateTime ReleaseDate { get; set; }
         public string Description { get; set; }
         public bool Accepted { get; set; }
-        public double AvgRating { get; set; }
     }
 }
